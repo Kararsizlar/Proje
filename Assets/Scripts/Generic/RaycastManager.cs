d716fe8632830afdd9161a1b044196da9336649d
That0neDev <sanlikaaneren@gmail.com>
2023-04-17 18:38:56 +0300

    Object Movement Fix

--- Assets/Scripts/Generic/RaycastManager.cs
+++ Assets/Scripts/Generic/RaycastManager.cs
@@ -6,10 +6,6 @@ public class RaycastManager : MonoBehaviour
 {
     [SerializeField] Camera currentCamera;
 
-    private Camera GetCamera(){
-        return Camera.current;
-    }
-
     public GameObject GetObjectFromCamera(Vector3 endPos,out RaycastHit[] results,float distance,LayerMask mask){       
         Vector3 startPoint = currentCamera.transform.position;
         Vector3 direction = (endPos - startPoint).normalized;
@@ -20,38 +16,5 @@ public class RaycastManager : MonoBehaviour
             return null;
         
         return results[0].collider.gameObject;
-    }
-
-    
-
-    public float GetFirstCollisionDistanceFromCamera(Vector3 endPos,float maxDistance,out bool isSafe,MeshCollider collider){    
-        if(currentCamera == null)
-            currentCamera = GetCamera();
-        
-
-        bool IsSafe(){
-            Bounds bounds = collider.bounds;
-            float xSize = bounds.size.x;
-            float ySize = bounds.size.y;
-            float zSize = bounds.size.z;
-            bool hit = Physics.BoxCast(endPos,new Vector3(xSize,ySize,zSize),Vector3.zero,Quaternion.identity);
-
-            return !hit;
-        }
-
-        Vector3 startPoint = currentCamera.transform.position;
-        Vector3 direction = (endPos - startPoint).normalized;
-        RaycastHit[] results = Physics.RaycastAll(startPoint,direction,maxDistance);
-        isSafe = IsSafe();
-
-        if(results.Length < 2)
-            return maxDistance;
-        
-        float distance = (results[1].point - currentCamera.transform.position).magnitude;
-
-        if(distance > maxDistance)
-            return maxDistance;
-        
-        return distance;
     }
 }
--- Assets/Scripts/ObjectHolder.cs
+++ Assets/Scripts/ObjectHolder.cs
@@ -7,26 +7,30 @@ public class ObjectHolder : MonoBehaviour
 {
     [Header("In-Game data")]
     [SerializeField] GameObject currentObject = null;
-    [SerializeField] MeshCollider currentCollider = null;
+    [SerializeField] Rigidbody currentBody = null;
     [SerializeField] float distanceFromCamera;
-    [SerializeField] float lastSafe;
+    [SerializeField] Vector3 mouseToWorld;
 
     [Header("Mouse Data")]
     [SerializeField] Vector2 mousePixels = Vector2.zero;
+    [SerializeField] Vector2 mouseChange = Vector2.zero;
     [SerializeField] bool holding = false;
     [SerializeField] bool clicking = false;
     
     [Header("Camera Distance Data")]
-    [SerializeField] float minDistance;
-    [SerializeField] float maxDistance;
+    [SerializeField] float allowedZMin;
+    [SerializeField] float allowedZMax;
 
     [Header("Other Requirements")]
     [SerializeField] RaycastManager raycastManager;
     [SerializeField] LayerMask holdableMask;
     [SerializeField] Camera cameraObject;
+    [SerializeField] float forcePower;
 
     public void GetMousePos(InputAction.CallbackContext context){
-        mousePixels = context.ReadValue<Vector2>();
+        Vector2 newValue = context.ReadValue<Vector2>();
+        mouseChange = newValue - mousePixels;
+        mousePixels = newValue;
     }
 
     public void GetClick(InputAction.CallbackContext context){
@@ -35,46 +39,66 @@ public class ObjectHolder : MonoBehaviour
         if(context.started)
             GetSelectedObject();
         
-        if(clicking == false && holding)
+        if(clicking == false && holding){
+            currentBody.useGravity = true;
+            currentObject = null;
+            currentBody = null;
             holding = false;
+        }
     }
 
     public void GetMiddleMouse(InputAction.CallbackContext context){
         int y = (int)context.ReadValue<Vector2>().y;
-        distanceFromCamera = Mathf.Clamp(distanceFromCamera + y,minDistance,maxDistance);
+        distanceFromCamera = Mathf.Clamp(distanceFromCamera + y,allowedZMin,allowedZMax);
     }
 
     private Vector3 GetMousePosWorld(Vector2 input){
-        Vector3 mouseVector3 = new Vector3(input.x,input.y,cameraObject.nearClipPlane);
+        Vector3 mouseVector3;
+
+        if(currentObject == null)
+            mouseVector3 = new Vector3(input.x,input.y,cameraObject.nearClipPlane);
+        else{
+            float diff = currentObject.transform.position.z - cameraObject.transform.position.z;
+            mouseVector3 = new Vector3(input.x,input.y,diff);
+        }
+           
         return cameraObject.ScreenToWorldPoint(mouseVector3);
     }
 
     public void GetSelectedObject(){   
         RaycastHit[] value = null;
-        currentObject = raycastManager.GetObjectFromCamera(GetMousePosWorld(mousePixels),out value,Mathf.Infinity,holdableMask);
-        holding = currentObject != null; 
+        currentObject = raycastManager.GetObjectFromCamera(mouseToWorld,out value,Mathf.Infinity,holdableMask);
+        holding = currentObject != null;
 
-        if(holding)
-            currentCollider = currentObject.GetComponent<MeshCollider>();
+        if(holding){
+            currentBody = currentObject.GetComponent<Rigidbody>(); 
+            currentBody.useGravity = false;
+        }
     }
 
     private void SetPositionOfObject(){
-        Vector3 direction = (GetMousePosWorld(mousePixels) - cameraObject.transform.position).normalized;
-        bool safe = false;
-        float distance = raycastManager.GetFirstCollisionDistanceFromCamera(GetMousePosWorld(mousePixels),distanceFromCamera,out safe,currentCollider);
-        Vector3 position;
-        if(safe){
-            position = direction * distance;
-            lastSafe = distance;
-        }
-        else
-            position = direction * lastSafe;
+        
+        Vector2 directionV2 = ((Vector2)mouseToWorld - (Vector2)currentObject.transform.position);
 
-        currentObject.transform.position = cameraObject.transform.position + position;
+        Vector2 direction = directionV2.normalized;
+        Vector2 force = forcePower * direction;
+        
+        float distance = directionV2.magnitude;
+
+        if(distance > 0.5f)
+            currentBody.AddForce(new Vector3(force.x,force.y,0));
+        
+        currentBody.MovePosition(new Vector3(currentBody.position.x,currentBody.position.y,distanceFromCamera));
     }
 
     public void Update(){
         if(holding)
             SetPositionOfObject();
+
+        mouseToWorld = GetMousePosWorld(mousePixels);
+    }
+
+    private void Awake(){
+        distanceFromCamera = allowedZMin;
     }
 }
